86afc0a15cf8d6bd7ffcd45d5326297bd72c707b
yaaaaashiki <yaaaaakishi@gmail.com>
2018-01-13 10:58:03 +0900

    Use external package for getting ping status

---
+++ helper/getWifiStatus.go
@@ -0,0 +1,7 @@
+package helper
+/*
+func GetWifiStatus() {
+	 := PingStaticIP()
+
+}
+*/
--- helper/pingStaticIP.go
+++ helper/pingStaticIP.go
@@ -1,18 +1,29 @@
-package helper
+package main
 
 import (
-	"os"
-	"os/exec"
+	"fmt"
+
+	ping "github.com/sparrc/go-ping"
 )
 
 const (
-	ping     = "ping"
-	staticIP = "127.0.0.1"
+	staticIP = "127.0.0.2"
 )
 
-func PingStaticIP() {
-	res := exec.Command(ping, staticIP)
-	res.Stdout = os.Stdout
-	res.Stderr = os.Stderr
-	res.Run()
+//PingStaticIP
+func main() {
+	/*
+		res := exec.Command(ping, staticIP)
+		res.Stdout = os.Stdout
+		res.Stderr = os.Stderr
+		res.Run()
+	*/
+
+	pinger, err := ping.NewPinger(staticIP)
+	if err != nil {
+		fmt.Fprintf(err)
+	}
+	pinger.Count = 3
+	pinger.Run()                 // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
 }
