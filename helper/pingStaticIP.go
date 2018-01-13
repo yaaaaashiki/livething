3b17aea230a35dc8fa74cfe5ded9556feec28bf9
yaaaaashiki <yaaaaakishi@gmail.com>
2018-01-13 11:32:44 +0900

    Remove unused code

--- helper/pingStaticIP.go
+++ helper/pingStaticIP.go
@@ -12,19 +12,12 @@ const (
 )
 
 func PingStaticIP() *ping.Statistics {
-	/*
-		res := exec.Command(ping, staticIP)
-		res.Stdout = os.Stdout
-		res.Stderr = os.Stderr
-		res.Run()
-	*/
-
 	pinger, err := ping.NewPinger(staticIP)
 	if err != nil {
 		log.Println(err)
 	}
 	pinger.Count = 3
-	pinger.Run()                 // blocks until finished
+	pinger.Run()               // blocks until finished
 	res := pinger.Statistics() // get send/receive/rtt stats
 	fmt.Println(res)
 	return res
