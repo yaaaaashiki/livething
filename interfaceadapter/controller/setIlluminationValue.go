4628984780ffa1a2a312d66bf934961d7f73f25a
yaaaaashiki <yaaaaakishi@gmail.com>
2018-01-13 13:09:27 +0900

    Refactor set illumination function

--- interfaceadapter/controller/setIlluminationValue.go
+++ interfaceadapter/controller/setIlluminationValue.go
@@ -1,31 +1,26 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"github.com/yaaaaashiki/cstack/helper"
+	"github.com/yaaaaashiki/livething/helper"
 )
 
 type Illumination struct {
 	Value int `binding:"required" json:"value"`
 }
 
-func Execute(c *gin.Context) {
-	//rawIlluminationValue, err := strconv.Atoi(c.Param("illumination"))
-	//IlluminationValue := uint(rawIlluminationValue)
+func setCurrentIlluminationValue(c *gin.Context) {
 
-	if err != nil {
+	illumination := &Illumination{}
+
+	if err := c.MustBindWith(illumination, binding.JSON); err != nil {
 		helper.ResponseErrorJSON(c, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	val := &IlluminationValue{}
-
-	if err := c.MustBindWith(val, binding.JSON); err != nil {
-		helper.ResponseErrorJSON(c, http.StatusBadRequest, err.Error())
-		return
-	}
-
+	fmt.Println(illumination.Value)
 }
