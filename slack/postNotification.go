43757dfe660f9cc79945a3674b5c35cd3eab43e6
yaaaaashiki <yaaaaakishi@gmail.com>
2018-01-14 20:41:20 +0900

    Add curl request specification

--- slack/postNotification.go
+++ slack/postNotification.go
@@ -1,6 +1,10 @@
 package slack
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/yaaaaashiki/livething/interfaceadapter/controller"
@@ -8,50 +12,77 @@ import (
 )
 
 const (
-	channelName               = "#notification"
-	userName                  = "livething"
-	returningHomeText         = "Welcome home!!!"
-	iconName                  = "warning"
-	postURI                   = "https://hooks.slack.com/services/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"
-	checkIlluminationInterval = 60
-	checkWifiInterval
+	channelName          = "#notification"
+	userName             = "livething"
+	returningHomeText    = "Welcome home!!!"
+	iconName             = "warning"
+	postURI              = "https://hooks.slack.com/services/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"
+	illuminationInterval = 60
+	wifiInterval
 	zeroValue      = 0
+	firstTime      = 0
 	alertRoopTimes = 5
-	firstTime      = 1
 )
 
-func setAlertText(objectName string) (alertText string) {
-	alertText = "Put" + objectName + "on the home position"
-	return
+var alertText string
+
+type Slack struct {
+	text        string `json:"text"`
+	userName    string `json:"username"`
+	iconName    string `json:"icon_emoji"`
+	channelName string `json:"channel"`
 }
 
-func PostNotification() {
+func setAlertText(objectName string) string {
+	alertText = "Put" + objectName + "on the home position"
+	return alertText
+}
+
+func curlRequest(text string) {
+	params, _ := json.Marshal(Slack{
+		text:        text,
+		username:    userName,
+		iconName:    iconName,
+		iconURI:     "",
+		channelName: channelName})
+
+	resp, _ := http.PostForm(
+		postURI,
+		url.Values{"payload": {string(params)}},
+	)
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+}
+
+func PostNotification(objectName string) {
 
 	roopTimesCounter := zeroValue
 
 	for {
 		if wifi.Status == false {
-			time.Sleep(checkWifiInterval * time.Second)
+			time.Sleep(wifiInterval * time.Second)
 			continue
 		}
 
 		if roopTimesCounter >= alertRoopTimes {
-			//post transaction to slack
+			setAlertText(objectName)
+			curlRequest(alertText)
+			time.Sleep(illuminationInterval * time.Second)
 			roopTimesCounter = zeroValue
 			continue
 		}
 
 		if roopTimesCounter == firstTime {
-			//post welcome notiridatin to slack
+			curlRequest(returningHomeText)
+			time.Sleep(illuminationInterval * time.Second)
+			roopTimesCounter++
 			continue
 		}
 
 		if controller.ObjectStatus == false {
+			time.Sleep(illuminationInterval * time.Second)
 			roopTimesCounter++
 		}
 	}
-
-	/*
-		curl -X POST --data-urlencode "payload={\"channel\": \"#notification\", \"username\": \"livething\", \"text\": \"This is posted to #notification and comes from a bot named webhookbot.\", \"icon_emoji\": \":warning:\"}"
-	*/
 }
